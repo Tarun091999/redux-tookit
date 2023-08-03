1d1efbffa29e8f5fc4eb9ddf844ec4f0cbcbc501
Tarun Khurana <tarunkhurana8398@example.com>
2023-08-03 14:56:34 +0530

    redux counter

--- src/App.js
+++ src/App.js
@@ -1,23 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import React from "react";
+import { Button } from "@mui/material";
+import { useSelector, useDispatch } from "react-redux";
+import { decrement, increment } from "./redux/slices/counter";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Badge from '@mui/material/Badge';
+import MailIcon from '@mui/icons-material/Mail';
+
 
 function App() {
+  const count = useSelector((state) => state.counter);
+  const dispatch = useDispatch();
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div className="container my-5">
+       <h1> Redux ToolKit ... </h1>
+       <hr/>
+      <div>
+      <Badge badgeContent={count} color="primary">
+       <MailIcon />
+      </Badge>
+        <Button
+          variant="contained"
+          aria-label="Increment value"
+          onClick={() => dispatch(increment())}
+          className="mx-3"
         >
-          Learn React
-        </a>
-      </header>
+          Increment
+        </Button>
+
+        <Button
+          variant="outlined"
+          aria-label="Decrement value"
+          onClick={() => dispatch(decrement())}
+        >
+          Decrement
+        </Button>
+      </div>
     </div>
   );
 }
